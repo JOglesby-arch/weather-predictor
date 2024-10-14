d7435752b69789d8fa62bd7554e2692f23638751
Jacob Oglesby <jacoboglesby15@gmail.com>
2024-10-14 14:49:21 -0500

    Removed API key because Open-Meteo requires no API key.
    sys.path.append was also unnecessary so it was removed as well.

--- modules/api_handler.py
+++ modules/api_handler.py
@@ -1,11 +1,6 @@
 # modules/api_handler.py
-import sys
-import os 
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import requests
-from keys import WEATHER_API_KEY
 
 def get_weather_data(location, days):
     base_url = "https://api.open-meteo.com/v1/forecast"
@@ -13,8 +8,7 @@ def get_weather_data(location, days):
         "latitude": location['latitude'],
         "longitude": location['longitude'],
         "hourly": "temperature_2m",
-        "forecast_days": days,
-        "apikey": WEATHER_API_KEY
+        "forecast_days": days
     }
     response = requests.get(base_url, params=params)
     if response.status_code == 200:
